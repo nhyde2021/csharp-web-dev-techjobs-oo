ac3eb4805e345c8d6ad78b302831118aee7681f3
Neil Hyde <neil.hyde17@gmail.com>
2021-12-01 16:38:12 -0600

    added TestInitialize to JobTests

--- TechJobsTest/JobTests.cs
+++ TechJobsTest/JobTests.cs
@@ -6,23 +6,59 @@ namespace TechJobsTest
     [TestClass]
     public class JobTests
     {
+        Job testJob1;
+        Job testJob2;
+
+        Employer testEmployer3;
+        Location testLocation3;
+        PositionType testPositionType3;
+        CoreCompetency testCoreCompetency3;
+        Job testJob3;
+
+        Employer testEmployer4;
+        Location testLocation4;
+        PositionType testPositionType4;
+        CoreCompetency testCoreCompetency4;
+        Job testJob4;
+
+        Employer testEmployer5;
+        Location testLocation5;
+        PositionType testPositionType5;
+        CoreCompetency testCoreCompetency5;
+        Job testJob5;
+
+        [TestInitialize]
+        public void CreateJobObjects()
+        {
+            testJob1 = new Job();
+            testJob2 = new Job();
+
+            testEmployer3 = new Employer("ACME");
+            testLocation3 = new Location("Desert");
+            testPositionType3 = new PositionType("Quality Control");
+            testCoreCompetency3 = new CoreCompetency("Persistence");
+            testJob3 = new Job("Product tester", testEmployer3, testLocation3, testPositionType3, testCoreCompetency3);
+
+            testEmployer4 = new Employer("Amazon");
+            testLocation4 = new Location("Jeffy B's place");
+            testPositionType4 = new PositionType("Management");
+            testCoreCompetency4 = new CoreCompetency("Smartness");
+            testJob4 = new Job("Lead IT Guy", testEmployer4, testLocation4, testPositionType4, testCoreCompetency4);
+
+            testEmployer5 = new Employer("Amazon");
+            testLocation5 = new Location("Jeffy B's place");
+            testPositionType5 = new PositionType("Management");
+            testCoreCompetency5 = new CoreCompetency("Smartness");
+            testJob5 = new Job("Lead IT Guy", testEmployer5, testLocation5, testPositionType5, testCoreCompetency5);
+        }
         [TestMethod]
         public void TestSettingJobId()
         {
-            Job testJob1 = new Job();
-            Job testJob2 = new Job();
-
             Assert.IsTrue(testJob1.Id != testJob2.Id);
         }
         [TestMethod]
         public void TestJobConstructorSetsAllFields()
         {
-            Employer testEmployer3 = new Employer("ACME");
-            Location testLocation3 = new Location("Desert");
-            PositionType testPositionType3 = new PositionType("Quality Control");
-            CoreCompetency testCoreCompetency3 = new CoreCompetency("Persistence");
-            Job testJob3 = new Job("Product tester", testEmployer3, testLocation3, testPositionType3, testCoreCompetency3);
-
             Assert.AreEqual("Product tester", testJob3.Name);
             Assert.AreEqual("ACME", testJob3.EmployerName.Value);
             Assert.AreEqual("Desert", testJob3.EmployerLocation.Value);
@@ -32,18 +68,6 @@ namespace TechJobsTest
         [TestMethod]
         public void TestJobsForEquality()
         {
-            Employer testEmployer4 = new Employer("Amazon");
-            Location testLocation4 = new Location("Jeffy B's place");
-            PositionType testPositionType4 = new PositionType("Management");
-            CoreCompetency testCoreCompetency4 = new CoreCompetency("Smartness");
-            Job testJob4 = new Job("Lead IT Guy", testEmployer4, testLocation4, testPositionType4, testCoreCompetency4);
-
-            Employer testEmployer5 = new Employer("Amazon");
-            Location testLocation5= new Location("Jeffy B's place");
-            PositionType testPositionType5 = new PositionType("Management");
-            CoreCompetency testCoreCompetency5 = new CoreCompetency("Smartness");
-            Job testJob5 = new Job("Lead IT Guy", testEmployer5, testLocation5, testPositionType5, testCoreCompetency5);
-
             Assert.IsFalse(Equals(testJob4, testJob5));
         }
 
