5f91ddd0f188bd27484404d71077a9e6b6468e24
Neil Hyde <neil.hyde17@gmail.com>
2021-12-06 23:56:06 -0600

    added JobFields parent class and DRY the child classes

---
+++ TechJobsOO/JobFields.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TechJobsOO
+{
+    public abstract class JobFields
+    {
+        public int Id { get; }
+        private static int nextId = 1;
+        public string Value { get; set; }
+
+        public JobFields()
+        {
+            Id = nextId;
+            nextId++;
+        }
+
+        public JobFields(string value) : this()
+        {
+            Value = value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is JobFields type &&
+                Id == type.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+    }
+}
--- TechJobsOO/PositionType.cs
+++ TechJobsOO/PositionType.cs
@@ -1,37 +1,20 @@
 ﻿using System;
 namespace TechJobsOO
 {
-    public class PositionType
+    public class PositionType : JobFields
     {
         public int Id { get; }
         private static int nextId = 1;
         public string Value { get; set; }
 
-        public PositionType()
-        {
-            Id = nextId;
-            nextId++;
-        }
 
-        public PositionType(string value) : this()
+        public PositionType(string value) : base(value)
         {
             Value = value;
         }
 
-        public override bool Equals(object obj)
-        {
-            return obj is PositionType type &&
-                Id == type.Id;
-        }
 
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(Id);
-        }
-        public override string ToString()
-        {
-            return this.Value;
-        }
+       
 
         // TODO: Add custom Equals(), GetHashCode(), and ToString() methods.
 
